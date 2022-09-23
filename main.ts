3387de4bb6faff9f78c106e94c6bf4fac78a0c58
sillyangel <sillyangel3@gmail.com>
2022-09-23 15:56:15 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,35 @@
 enum RadioMessage {
     message1 = 49434
 }
+
+// input
 input.onButtonPressed(Button.A, function () {
-    led.toggle(0, 2)
-    basic.pause(200)
-    led.toggle(1, 2)
-    basic.pause(200)
-    led.toggle(2, 2)
-    basic.pause(200)
-    led.toggle(3, 2)
-    basic.pause(200)
-    led.toggle(4, 2)
-    basic.pause(200)
-    led.toggle(0, 2)
-    basic.pause(200)
-    led.toggle(1, 2)
-    basic.pause(200)
-    led.toggle(2, 2)
-    basic.pause(200)
-    led.toggle(3, 2)
-    basic.pause(200)
-    led.toggle(4, 2)
+	loadingscreen()
 })
 input.onButtonPressed(Button.B, function () {
     led.toggle(4, 2)
     basic.pause(200)
     led.toggle(4, 2)
 })
+// functions
+function loadingscreen() {
+    led.toggle(0, 2)
+    basic.pause(200)
+    led.toggle(1, 2)
+    basic.pause(200)
+    led.toggle(2, 2)
+    basic.pause(200)
+    led.toggle(3, 2)
+    basic.pause(200)
+    led.toggle(4, 2)
+    basic.pause(200)
+    led.toggle(0, 2)
+    basic.pause(200)
+    led.toggle(1, 2)
+    basic.pause(200)
+    led.toggle(2, 2)
+    basic.pause(200)
+    led.toggle(3, 2)
+    basic.pause(200)
+    led.toggle(4, 2)
+}
